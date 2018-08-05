2290c7a19fdc9d141462e53f1685f26a103dcd3e
Chris Fenos <chris.m.fenos@gmail.com>
2018-08-05 15:58:26 -0400

    Validation algorithm further expanded and still checks basic language

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,22 +1,52 @@
 const swearjar = require('swearjar')
 const sentencePerBlock = 3
-let blocks = []
 
 exports.validate = function(submissionId, content) {
-  createBlocks(submissionId, content)
+ const blockSet = createBlocks(submissionId, content)
+ console.log(blockSet)
   return "hello"
 };
 
+/**
+ * Loop through blocks -> Max (total sentences / # sentences per block)
+ * Loop through each block and push content to block
+ * Verify that no blocks have empty content
+ */
 createBlocks = (submissionId, content) => {
-  var sentences = (content).split('.')
-  for(let i = 0; i < Math.ceil(sentences.length/sentencePerBlock); i++) {
-    let block = {}
-    block.submissionID = submissionId
-    block.blockNumber = i
-    // for(let x = 0; x < sentencePerBlock; x++) {
-    //   block.content.push(sentences[(i * sentencePerBlock) + x])
-    // }
-    blocks.push(block)
-    console.log(blocks);
+  const sentences = (content).split('.')
+  let blockCollection = {} //this holds all the blocks and their status
+  blockCollection.approved = false
+  blockCollection.submissionId = submissionId
+  blockCollection.blocks = []
+  for(let totalBlocks = 0; totalBlocks < Math.ceil(sentences.length/sentencePerBlock); totalBlocks++) {
+    let block = {} //create new block
+    block.currentBlockIndex = totalBlocks //gives block an index of total blocks created
+    block.content = []
+    block.flags = []
+    for(let sentence = 0; sentence < sentencePerBlock; sentence++) {
+      const currentLine = ((totalBlocks * sentencePerBlock) + sentence)      
+      if(sentences[currentLine] == null || sentences[currentLine] == undefined || sentences[currentLine] == "") {
+        break
+      } else {      
+        block.content.push(sentences[currentLine])
+      }
+    }
+    flagBadBlocks(block)
+    blockCollection.blocks.push(block)
+  }
+  return blockCollection
+}
+
+flagBadBlocks = (block) => {
+  for(var i = 0; i < block.content.length; i++) { 
+    var flaggedContent = swearjar.scorecard(block.content[i]);
+    var wordFlags = {}
+    for (var badContentType in flaggedContent) {
+      wordFlags.word = block.content[i];
+      if (flaggedContent.hasOwnProperty(badContentType)) {
+          wordFlags.type = badContentType;             
+          block.flags.push({ "type": wordFlags.type, "sentenceIndex": i})                
+      }     
+    }
   }
 }
