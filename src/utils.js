3f3e145b489fd0333b3d5ff0cf343ed97e7b57ea
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2018-09-12 12:09:07 -0400

    Updated token validation

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,23 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
-function getUserId(context) {
+function getAccountId(context) {
   const Authorization = context.request.get('Authorization')
   if (Authorization) {
     const token = Authorization.replace('Bearer ', '')
-    const userId = jwt.verify(token, process.env.TOKEN_SECRET);
-    return userId
+    const accountId = jwt.verify(token, process.env.TOKEN_SECRET);
+    return accountId
   }
 
-  throw new Error('User is not authenticated')
+  throw new AuthError()
+}
+
+class AuthError extends Error {
+  constructor() {
+    super('Not Authorized')
+  }
 }
 
 module.exports = {
-  getUserId,
+  getAccountId,
 }
