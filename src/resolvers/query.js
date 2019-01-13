21bfe9a12063a20ee9d5bfa5b9e8f4fe4a683f6a
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-01-12 20:49:18 -0500

    feat: addded pagination to list queries

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,136 +1,181 @@
-// Author: Austin Howlett, Gerhard Knelsen
-// Description: Responsible for resolving all query (in relation to REST this would GET endpoints) schema endpoints (business logic)
-
 const { verifyToken } = require('../utils');
 const { storyFragment } = require("../fragments/StoryFragment");
 const { contributionFragment } = require("../fragments/contributionFragment");
 const { revisionFragment } = require("../fragments/RevisionFragment");
 
-async function allStories (_, args, context, info) {
-    // const payload = verifyToken(context)
-    return await context.prisma.stories({
-        orderBy: 'id_DESC'
-    }).$fragment(storyFragment)
+allStories = async (_, args, context, info) => {
+  return await context.prisma.stories({
+    orderBy: 'id_DESC',
+    first: args.first,
+    last: args.last,
+    before: args.before,
+    after: args.after,
+    skip: args.skip,
+  }).$fragment(storyFragment)
 }
 
-function stories (_, args, context, info) {
-    const payload = verifyToken(context)
-    return context.prisma.stories({
-        where: {
-            OR: [
-                { title: args.searchString },
-                { description: args.searchString },
-            ]
-        },
-    }).$fragment(storyFragment)
+stories = (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return context.prisma.stories({
+    where: {
+      OR: [
+        { title: args.searchString },
+        { description: args.searchString },
+      ],
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip,
+    },
+  }).$fragment(storyFragment)
 }
 
-async function storyById (_, args, context, info) {
-    const payload = verifyToken(context)
-    return await context.prisma.story({ id: args.storyID }).$fragment(storyFragment)
+storyById = async (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return await context.prisma.story({ id: args.storyID }).$fragment(storyFragment)
 }
 
-function storiesByAuthorId (_, args, context, info) {
-    const payload = verifyToken(context)
-    return context.prisma.stories({
-        where: {
-            AND: [
-                { authorId: args.authorId },
-                { isForked: false },
-                { isCloned: false }
-            ]
+storiesByAuthorId = (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return context.prisma.stories({
+    where: {
+      AND: [
+        { authorId: args.authorId },
+        { isForked: false },
+        { isCloned: false }
+      ],
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip,
+    }
+  }).$fragment(storyFragment)
+}
+
+storiesByNonAuthorId = (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return context.prisma.stories({
+    where: {
+      AND: [
+        { nonAuthorId: args.nonAuthorId },
+        {
+          OR: [
+            { isForked: true },
+            { isCloned: true }
+          ]
         }
-    }).$fragment(storyFragment)
+      ],
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip,
+    }
+  }).$fragment(storyFragment)
 }
 
-function storiesByNonAuthorId(_, args, context, info) {
-    const payload = verifyToken(context)
-    return context.prisma.stories({
-        where: {
-            AND: [
-                { nonAuthorId: args.nonAuthorId },
-                {
-                    OR: [
-                        { isForked: true },
-                        { isCloned: true }
-                    ]
-                }
-            ]
-        }
-    }).$fragment(storyFragment)
+storiesByCommunityId = (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return context.prisma.stories({
+    where: {
+      communityId: args.communityId,
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip,
+    }
+  }).$fragment(storyFragment)
 }
 
-function storiesByCommunityId(_, args, context, info) {
-    const payload = verifyToken(context)
-    return context.prisma.stories({
-        where: {
-            communityId: args.communityId
-        }
-    }).$fragment(storyFragment)
+searchByKeyword = (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return context.prisma.stories({
+    where: {
+      OR: [
+          { title_contains: args.searchString },
+          { description_contains: args.searchString },
+          { content_contains: args.searchString },
+      ],
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip,
+    }
+  }).$fragment(storyFragment)
 }
 
-function searchByKeyword (_, args, context, info) {
-    const payload = verifyToken(context)
-    return context.prisma.stories({
-        where: {
-            OR: [
-                { title_contains: args.searchString },
-                { description_contains: args.searchString },
-                { content_contains: args.searchString },
-            ]
-        }
-    }).$fragment(storyFragment)
+searchByTitle = (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return context.prisma.stories({
+    where: {
+      OR: [
+        { title_contains: args.searchString },
+      ],
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip,
+    }
+  }).$fragment(storyFragment)
 }
 
-function searchByTitle (_, args, context, info) {
-    const payload = verifyToken(context)
-    return context.prisma.stories({
-        where: {
-            OR: [ { title_contains: args.searchString }, ]
-        }
-    }).$fragment(storyFragment)
+searchByDescription = (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return context.prisma.stories({
+    where: {
+      OR: [
+        { title_description: args.searchString },
+      ],
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip,
+    }
+  }).$fragment(storyFragment)
 }
 
-function searchByDescription (_, args, context, info) {
-    const payload = verifyToken(context)
-    return context.prisma.stories({
-        where: {
-            OR: [ { title_description: args.searchString }, ]
-        }
-    }).$fragment(storyFragment)
+getContributionsById = (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return context.prisma.contributions({
+    where: {
+      authorProfileId: args.authorProfileId,
+      first: args.first,
+      last: args.last,
+      before: args.before,
+      after: args.after,
+      skip: args.skip,
+    }
+  }).$fragment(contributionFragment)
 }
 
-function getContributionsById(_, args, context, info) {
-    const payload = verifyToken(context)
-    return context.prisma.contributions({
-        where: {
-            authorProfileId: args.authorProfileId
-        }
-    }).$fragment(contributionFragment)
-}
-
-async function getContributionById(_, args, context, info) {
+getContributionById = async (_, args, context, info) => {
     const payload = verifyToken(context)
     return await context.prisma.contribution({ id: args.id })
         .$fragment(contributionFragment)
 }
 
-async function revisionById (_, args, context, info) {
+revisionById = async (_, args, context, info) => {
     const payload = verifyToken(context)
     return await context.prisma.revision({ id: args.id }).$fragment(revisionFragment)
 }
 
 module.exports = {
-    allStories,
-    stories,
-    storyById,
-    storiesByAuthorId,
-    storiesByNonAuthorId,
-    searchByKeyword,
-    searchByTitle,
-    searchByDescription,
-    getContributionsById,
-    getContributionById,
-    storiesByCommunityId,
-    revisionById
+  allStories,
+  stories,
+  storyById,
+  storiesByAuthorId,
+  storiesByNonAuthorId,
+  searchByKeyword,
+  searchByTitle,
+  searchByDescription,
+  getContributionsById,
+  getContributionById,
+  storiesByCommunityId,
+  revisionById
 }
